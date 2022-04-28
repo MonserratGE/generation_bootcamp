ee16c1bbbebf38035e0f794870bcc54837996c6b
MonserratGE <monserrat.garcia.espinoza32@gmail.com>
2022-04-28 15:52:09 -0500

    Ejercicios de Ciclos

---
+++ JavaScript/ciclos/assets/js/ejercicios.js
@@ -0,0 +1,49 @@
+
+//escribe un ciclo anidado que imprima el siguiente patrón.
+
+/*  
+*  
+ * *  
+ * * *  
+ * * * *  
+ * * * * * 
+ */
+
+
+/* for(let i = 1; i <= 5; i++){ //1 //2
+    for(let j = 1; j <= i; j++){ //5 //5
+        document.write('*');
+        //console.log('ciclo hijo\n');
+    }
+    
+    document.write('<br>')
+} */
+
+/* let numIn = new Number(prompt("Ingrese un número"));
+
+for (numIn ; numIn>0 ; numIn--) {
+    document.write("<hr>" + numIn + "<hr>");
+    for (let i = 0; i<=numIn; i++) {
+        let arrayIn = new Array();
+        arrayIn.push(i);
+        suma = arrayIn + ;
+        document.write(suma + "<br>");
+    }
+}
+
+const sam = () => {
+    let numIn = new Number(prompt("Ingrese un número"));
+    const res = [];
+    let total = 0;
+    for (numIn ; numIn>0 ; numIn--) {
+        res.push(numIn)
+        total += numIn;
+      }
+      document.write(res.length + "<hr>")
+      document.write(total + " = " + res.join(' + ') +"<hr>")
+      // document.write(total + "<hr>")
+  }
+  
+  sam() */
+  
+
---
+++ JavaScript/ciclos/assets/js/main.js
@@ -0,0 +1,35 @@
+/* Ciclos */
+
+// for
+// for ...in
+//.forEach
+
+//Imprimir la frase "Hola mundo" 10 veces
+
+/* for(let i = 1; i <= 10; i++){
+    console.log('Hola Mundo', i);
+    debugger;
+}
+ */
+// Iteraciones = una vuelta de cada ciclo
+
+// While
+
+/* let control = 1; //i
+
+while (control <= 5) { //condición a observar
+    console.log('Hola desde un ciclo while', control);
+    control ++; //modificador de la variable de control 
+}
+
+console.log(control); */
+
+//di ...while
+
+let i = 6;
+
+do {
+    console.log('Hola desde do..while', 1);
+    i++;
+} while (i <=5);
+
